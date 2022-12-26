c8b35aeb6a23b00711f04ea65ccbbc07b5eb8001
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-26 21:36:32 +0530

    [Jyoti Rao]:Inheritance Program

---
+++ Inheritance_demo/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_demo
+{
+    public class Employee
+    {
+       public int salary = 50000;
+    }
+    public class Employee2 : Employee
+    {
+        public int bonus=10000;
+    }
+}
---
+++ Inheritance_demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance_demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee2 emp = new Employee2();
+           int Salary= emp.salary;
+           int Bonus= emp.bonus;
+            Console.WriteLine("Salary of employee: " + Salary);
+            Console.WriteLine("Bonus of employee2: " + Bonus);
+
+        }
+    }
+}
