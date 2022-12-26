9133357e506283b1a7ec8efbc908303ab77cf11a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-26 21:27:42 +0530

    [Jyoti Rao]:Constructor Program

---
+++ constructor_demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using default_constructor_demo;
+using System;
+
+namespace constructor_demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle obj= new Rectangle();
+            Rectangle obj1 = new Rectangle(10,20);
+
+        }
+    }
+}
---
+++ constructor_demo/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace default_constructor_demo
+{
+    public class Rectangle
+    {
+        public Rectangle() //default constructor
+        {
+            int height = 6;
+            int width = 5;
+            int area = height * width;
+            Console.WriteLine("Area of default constructor:"+ area);
+        }
+        public Rectangle(int h,int w) //parameterized constructor
+        {
+            int height = h;
+            int width = w;
+            int area = height * width;
+            Console.WriteLine("Area of parameterized constructor: " + area);
+        }
+    }
+}
