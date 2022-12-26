26ab24dea9fd4b97600436ee8eef90d4f3a1ebea
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-26 21:32:11 +0530

    [Jyoti Rao]:Interface Program

---
+++ Interface_Demo/ArithmeticOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface_Demo
+{
+    public interface ArithmeticOperations
+    {
+        public void Addition();
+        public void Subtraction();
+        public void Multiplication();
+        public void Division();
+    }
+    class Calculations : ArithmeticOperations
+    {
+        public void Addition() 
+        {
+            int num1=5, num2=10;
+            int result=num1+num2;
+            Console.WriteLine("Additions result are: " + result);
+        }
+        public void Subtraction()
+        {
+            int num1 = 15, num2 = 10;
+            int result = num1 - num2;
+            Console.WriteLine("Subtraction result are: " + result);
+        }
+        public void Multiplication()
+        {
+            int num1 = 5, num2 = 10;
+            int result = num1 * num2;
+            Console.WriteLine("Multiplication result are: " + result);
+        }
+        public void Division()
+        {
+            double num1 = 255, num2 = 10;
+            double result = num1 / num2;
+            Console.WriteLine("Division result are: " + result);
+        }
+    }
+}
---
+++ Interface_Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interface_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculations obj= new Calculations();
+            obj.Addition();
+            obj.Subtraction();
+            obj.Division();
+            obj.Multiplication();
+        }
+    }
+}
